7a83fc75ed51c25c76fbbd6aabc7a476e59076d0
Luis Mora Lepin <lml24d@fsu.edu>
2024-06-12 20:04:09 -0500

    Adding continous monitoring

--- UPS/README.md
+++ UPS/README.md
@@ -1,5 +1,13 @@
 # UPS 
 
+## How to start the remote monitoring?
 Under development... 
 To test go to the Source directory and run UPS_library.py 
-## How to start the remote monitoring?
+This will start the continous monitoring of the UPS. For it
+only prints the information to the terminal.
+
+Next steps:
+
+1.- Send info to influxDB
+2.- Send info to the pc connected to Ignition 
+
--- UPS/Source/UPS_library.py
+++ UPS/Source/UPS_library.py
@@ -8,7 +8,8 @@ import sys
 
 class UPS(UNIT):
     '''
-    This class represents the template for a UPS system.
+    This class represents the template to
+    monitor an UPS system.
     '''
     def __init__(self):
         self.mib_dir = "../mibs_ups/"   # Need to declare a top directory for this 
@@ -16,6 +17,8 @@ class UPS(UNIT):
         self.battery_failure = self.get_battery_fail()
         self.battery_time = self.get_battery_time()
         self.battery_cap = self.get_battery_cap()
+        self.battery_v = self.get_battery_voltage()
+        self.battery_aged = self.get_battery_age()
 
     def get_battery_fail(self):
         ups_stat_var = "xupsBatteryFailure.0"
@@ -37,14 +40,40 @@ class UPS(UNIT):
         output_command = self.execute_command(command)
         temp_val = self.parse_snmpget_output(output_command)
         return temp_val['Value']
+    
+    def get_battery_voltage(self):
+        ups_stat_var = "xupsBatVoltage.0"
+        command = f"snmpget -v 3 -M +{self.mib_dir} -m ALL -u readonly -c public {self.device_address} {ups_stat_var}" 
+        output_command = self.execute_command(command)
+        temp_val = self.parse_snmpget_output(output_command)
+        return temp_val['Value']
+    
+    def get_battery_age(self):
+        ups_stat_var = "xupsBatteryAged.0"
+        command = f"snmpget -v 3 -M +{self.mib_dir} -m ALL -u readonly -c public {self.device_address} {ups_stat_var}" 
+        output_command = self.execute_command(command)
+        temp_val = self.parse_snmpget_output(output_command)
+        return temp_val['Value']
+    
+
+    def CONTINUOUS_monitoring(self):
+        '''
+        Description:    Continuously record timestamp on InfluxDB
+        '''
+        print("Monitoring UPS system...")
+        while True:
+            print('Actual time: ', datetime.now())
+            print('Battery failing? :', self.battery_failure['symbolic_name'])
+            print('Battery capacity: ', self.battery_cap['value'])
+            print('Battery voltage: ', self.battery_v['value'])
+            print('Remaining battery time: ', self.battery_time['value'])
+            time.sleep(10)
 
 
 
 ups_a = UPS()
-print(ups_a.device_address)
-print(ups_a.battery_failure)
-print(ups_a.battery_time)
-print(ups_a.battery_cap)
+ups_a.CONTINUOUS_monitoring()
+
 
 
 
