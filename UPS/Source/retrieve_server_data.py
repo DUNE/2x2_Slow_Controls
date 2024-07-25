031ab50ad65dca07a5b14c1874f21e79eca07e0e
Luis Mora Lepin <lml24d@fsu.edu>
2024-07-25 16:20:05 -0500

    Script to retrive vars is working

--- UPS/Source/retrieve_server_data.py
+++ UPS/Source/retrieve_server_data.py
@@ -1,48 +1,50 @@
 import asyncio
 from asyncua import Client
 
-async def main():
+async def retrieve_data():
     '''
     Function used to
     retrieve data from an OPC
     sever using asyncua
     '''
-
-    url="opc.tcp://0.0.0.0:4840/freeopcua/server/"
+    url="opc.tcp://192.168.197.91:4840/freeopcua/server/"
     # Create a client instance
     async with Client(url) as client:
         print("Connected to the server")
         
         # Get the root node
         root = client.nodes.root
-        print("Root node is: ", root)
+        #print("Root node is: ", root)
         
         # Browse the root node
         objects = await root.get_child(["0:Objects"])
-        print("Objects node is: ", objects)
+        #print("Objects node is: ", objects)
         
         # Browse for a specific object/node
         myobj = await objects.get_child(["2:MyObject"])
         print("MyObject node is: ", myobj)
         
         # Get a variable node
-        myvar = await myobj.get_child(["2:MyVariable"])
-        print("MyVariable node is: ", myvar)
+        myvar_date = await myobj.get_child(["2:DateVar"])
+        print("MyVariable node is: ", myvar_date)
         
         # Read the value of the variable
-        value = await myvar.read_value()
-        print("Current value of MyVariable is: ", value)
+        date = await myvar_date.read_value()
+
+        # Get a variable node
+        myvar_bat_time = await myobj.get_child(["2:BatTime"])
+        print("MyVariable node is: ", myvar_bat_time)
         
-        # Write a new value to the variable
-        await myvar.write_value(100)
-        print("Wrote new value to MyVariable")
+        # Read the value of the variable
+        bat_time = await myvar_bat_time.read_value()
+
+
+        print("Current date is: ", date)
+        print("Remaining battery time: ", bat_time)
         
-        # Read the value again to confirm the change
-        new_value = await myvar.read_value()
-        print("New value of MyVariable is: ", new_value)
     
 
 # Run the client script
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(retrieve_data())
 
