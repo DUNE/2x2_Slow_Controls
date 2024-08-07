81dd85f578045e288f872be30a09a616987351d1
Sungbin Oh <sungbino@acd-gw05.fnal.gov>
2024-08-07 14:29:48 -0500

    minor bug fix

--- epics/archiver/runarchiver.sh
+++ epics/archiver/runarchiver.sh
@@ -5,4 +5,4 @@
 source ~/2x2/EPICS/setup_EPICS.sh
 cd /home/acd/acdcs/2x2/EPICS/phoebus/phoebus/services/archive-engine
 
-./archive-engine.sh -engine ND2x2 -port 4812 -pluginCustomization /home/acd/acdcs/2x2/EPICS/2x2_Slow_Controls/epics/archiver/archiver.ini
+./archive-engine.sh -engine ND2x2 -port 4812 -settings /home/acd/acdcs/2x2/EPICS/2x2_Slow_Controls/epics/archiver/archiver.ini
