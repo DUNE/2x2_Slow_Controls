9136a6a782c3c3efbdfbf43ddff84c96711e6935
Sungbin Oh <sungbino@acd-gw05.fnal.gov>
2024-08-07 14:23:54 -0500

    Adding archiver engine related scripts

---
+++ epics/archiver/load_config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# NB: Make sure archiver_helper is using the correct "engine"
+#
+
+source ~/2x2/EPICS/setup_EPICS.sh
+cd /home/acd/acdcs/2x2/EPICS/phoebus/phoebus/services/archive-engine
+
+./archive-engine.sh -engine ND2x2 -port 4812 -import /home/acd/acdcs/2x2/EPICS/2x2_Slow_Controls/epics/archiver/config/archiver_config.xml -replace_engine
---
+++ epics/archiver/runarchiver.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# NB: Make sure archiver_helper is using the correct "engine"
+#
+
+source ~/2x2/EPICS/setup_EPICS.sh
+cd /home/acd/acdcs/2x2/EPICS/phoebus/phoebus/services/archive-engine
+
+./archive-engine.sh -engine ND2x2 -port 4812 -pluginCustomization /home/acd/acdcs/2x2/EPICS/2x2_Slow_Controls/epics/archiver/archiver.ini
