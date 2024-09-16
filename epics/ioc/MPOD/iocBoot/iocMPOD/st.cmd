a2d743c00190c39c0f3702f37b5192ce25ae9969
Sungbin Oh <sungbino@acd-daq05.fnal.gov>
2024-09-16 16:32:25 -0500

    Added GUI for all MPOD channels

--- epics/ioc/MPOD/iocBoot/iocMPOD/st.cmd
+++ epics/ioc/MPOD/iocBoot/iocMPOD/st.cmd
@@ -15,23 +15,23 @@ MPOD_registerRecordDeviceDriver pdbbase
 devSnmpSetParam("DebugLevel",1)
 devSnmpSetParam("SessionTimeout", "100000000")
 epicsEnvSet("WIENER_SNMP","COMMUNITY=guru,W=WIENER-CRATE-MIB")
-epicsEnvSet("ACD-MPOD1","HOST=acd-mpod1,crate=1")
-epicsEnvSet("ACD-MPOD2","HOST=acd-mpod2,crate=2")
+epicsEnvSet("ACD-MPOD1","HOST=acd-mpod1,crate=0")
+epicsEnvSet("ACD-MPOD2","HOST=acd-mpod2,crate=1")
 
 ## Crate
-#dbLoadRecords("db/crate.db","${ACD-MPOD1},${WIENER_SNMP}")
-#dbLoadRecords("db/crate.db","${ACD-MPOD2},${WIENER_SNMP}")
+dbLoadRecords("db/crate.db","${ACD-MPOD1},${WIENER_SNMP}")
+dbLoadRecords("db/crate.db","${ACD-MPOD2},${WIENER_SNMP}")
 
 ## Slots
-#dbLoadRecords("db/slot_slot0.db","${ACD-MPOD1},${WIENER_SNMP},slot=0")
-#dbLoadRecords("db/slot_slot1-3.db","${ACD-MPOD1},${WIENER_SNMP},slot=1")
-#dbLoadRecords("db/slot_slot1-3.db","${ACD-MPOD1},${WIENER_SNMP},slot=2")
-#dbLoadRecords("db/slot_slot1-3.db","${ACD-MPOD1},${WIENER_SNMP},slot=3")
+dbLoadRecords("db/slot_slot0.db","${ACD-MPOD1},${WIENER_SNMP},slot=0")
+dbLoadRecords("db/slot_slot1-3.db","${ACD-MPOD1},${WIENER_SNMP},slot=1")
+dbLoadRecords("db/slot_slot1-3.db","${ACD-MPOD1},${WIENER_SNMP},slot=2")
+dbLoadRecords("db/slot_slot1-3.db","${ACD-MPOD1},${WIENER_SNMP},slot=3")
 
-#dbLoadRecords("db/slot_slot0.db","${ACD-MPOD2},${WIENER_SNMP},slot=0")
-#dbLoadRecords("db/slot_slot1-3.db","${ACD-MPOD2},${WIENER_SNMP},slot=1")
-#dbLoadRecords("db/slot_slot1-3.db","${ACD-MPOD2},${WIENER_SNMP},slot=2")
-#dbLoadRecords("db/slot_slot1-3.db","${ACD-MPOD2},${WIENER_SNMP},slot=3")
+dbLoadRecords("db/slot_slot0.db","${ACD-MPOD2},${WIENER_SNMP},slot=0")
+dbLoadRecords("db/slot_slot1-3.db","${ACD-MPOD2},${WIENER_SNMP},slot=1")
+dbLoadRecords("db/slot_slot1-3.db","${ACD-MPOD2},${WIENER_SNMP},slot=2")
+dbLoadRecords("db/slot_slot1-3.db","${ACD-MPOD2},${WIENER_SNMP},slot=3")
 
 ## VGAs
 dbLoadTemplate("db/VGA_Mod0.sub","${ACD-MPOD1},${WIENER_SNMP}")
@@ -40,16 +40,16 @@ dbLoadTemplate("db/VGA_Mod2.sub","${ACD-MPOD2},${WIENER_SNMP}")
 dbLoadTemplate("db/VGA_Mod3.sub","${ACD-MPOD2},${WIENER_SNMP}")
 
 ## RTDs
-#dbLoadTemplate("db/RTD_Mod0.sub","${ACD-MPOD1},${WIENER_SNMP}")
-#dbLoadTemplate("db/RTD_Mod1.sub","${ACD-MPOD1},${WIENER_SNMP}")
-#dbLoadTemplate("db/RTD_Mod2.sub","${ACD-MPOD2},${WIENER_SNMP}")
-#dbLoadTemplate("db/RTD_Mod3.sub","${ACD-MPOD2},${WIENER_SNMP}")
+dbLoadTemplate("db/RTD_Mod0.sub","${ACD-MPOD1},${WIENER_SNMP}")
+dbLoadTemplate("db/RTD_Mod1.sub","${ACD-MPOD1},${WIENER_SNMP}")
+dbLoadTemplate("db/RTD_Mod2.sub","${ACD-MPOD2},${WIENER_SNMP}")
+dbLoadTemplate("db/RTD_Mod3.sub","${ACD-MPOD2},${WIENER_SNMP}")
 
 ## PACMAN
-#dbLoadTemplate("db/PAC_Mod0.sub","${ACD-MPOD1},${WIENER_SNMP}")
-#dbLoadTemplate("db/PAC_Mod1.sub","${ACD-MPOD1},${WIENER_SNMP}")
-#dbLoadTemplate("db/PAC_Mod2.sub","${ACD-MPOD2},${WIENER_SNMP}")
-#dbLoadTemplate("db/PAC_Mod3.sub","${ACD-MPOD2},${WIENER_SNMP}")
+dbLoadTemplate("db/PAC_Mod0.sub","${ACD-MPOD1},${WIENER_SNMP}")
+dbLoadTemplate("db/PAC_Mod1.sub","${ACD-MPOD1},${WIENER_SNMP}")
+dbLoadTemplate("db/PAC_Mod2.sub","${ACD-MPOD2},${WIENER_SNMP}")
+dbLoadTemplate("db/PAC_Mod3.sub","${ACD-MPOD2},${WIENER_SNMP}")
 
 #cd "${TOP}/iocBoot/${IOC}"
 iocInit
