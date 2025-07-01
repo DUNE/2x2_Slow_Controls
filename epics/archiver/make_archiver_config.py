46136fadc30d3d9d6f33e94d12f458f908847893
Sungbin Oh <sungbino@acd-daq05.fnal.gov>
2025-07-01 16:45:28 -0500

    adding MPOD PVs to archiver config

--- epics/archiver/make_archiver_config.py
+++ epics/archiver/make_archiver_config.py
@@ -32,7 +32,7 @@ def add_pv(filename, pvname, period, tier):
 
 
 #### == Write an .xml file
-filename = 'ND2x2_archiver_config_20250516.xml'    
+filename = 'ND2x2_archiver_config_20250701.xml'    
 f = open(filename,'w')
 f.write('<?xml version="1.0" encoding="UTF-8" standalone="no"?>')
 f.write(
@@ -55,6 +55,54 @@ end_group(filename, 1)
 start_group(filename, "SpellmanHV", 1)
 add_pv(filename, "spellmanhv/rI", "0.2", 2)
 add_pv(filename, "spellmanhv/rV", "0.2", 2)
+end_group(filename, 1)
+
+#################################
+### == add MPODs
+#################################
+channel_pvs = ["outputMeasurementCurrent", "outputMeasurementSenseVoltage", "outputMeasurementTerminalVoltage"] #, "outputStatus"]
+#crate_pvs = ["sysStatus"]
+start_group(filename, "MPOD", 1)
+for module in range(0, 4):
+    this_module = "Mod" + str(module)
+    ## -- Add VGA
+    for vga in range(1, 5):
+        this_vga = this_module + "-VGA_Card_" + str(vga)
+        for channel_pv in channel_pvs:
+            this_pv = this_vga + "/" + channel_pv
+            add_pv(filename, this_pv, "0.2", 2)
+
+    ## -- Add RTD
+    if module == 0:
+        for rtds in range(1, 3):
+            this_rtd = this_module + "-RTD_" + str(rtds)
+            for channel_pv in channel_pvs:
+                this_pv = this_rtd  + "/" + channel_pv
+                add_pv(filename, this_pv, "0.2", 2)
+    else:
+        this_rtd = this_module + "-RTD"
+        for channel_pv in channel_pvs:
+            this_pv = this_rtd  + "/" + channel_pv
+            add_pv(filename, this_pv, "0.2", 2)
+
+    ## -- Add PACFAN & PACMAN
+    for tpc in range(1, 3):
+        this_tpc = this_module + "-TPC" + str(tpc)
+
+        ## -- PACFAN & PACMAN
+        for channel_pv in channel_pvs:
+            this_pv = this_tpc  + "_pacFAN/" + channel_pv
+            add_pv(filename, this_pv, "0.2", 2)
+            this_pv = this_tpc  + "_PACMAN/" + channel_pv
+            add_pv(filename, this_pv, "0.2", 2)
+
+    ## interlock
+    if module != 2:
+        this_interlock = this_module + "-interlock"
+        for channel_pv in channel_pvs:
+            this_pv = this_interlock  + "_pacFAN/" + channel_pv
+            add_pv(filename, this_pv, "0.2", 2)
+            
 end_group(filename, 1)
 
 #################################
